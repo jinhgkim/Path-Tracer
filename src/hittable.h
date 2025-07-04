49fe01830f9b740bce4dfe2d9821a35716f45947
jinniekim <jinhgkim@gmail.com>
2025-07-03 21:21:56 -0700

    Add materials: lambertian, metal, dielectric

--- src/hittable.h
+++ src/hittable.h
@@ -5,11 +5,14 @@
 #include "ray.h"
 #include "vec3.h"
 
+class material;
+
 class hit_record
 {
   public:
     point3 p;
     vec3 normal;
+    shared_ptr<material> mat;
     double t;
     bool front_face;
 
